c1cb0420431e0b575b3afbeb7b16ef5a28a37f8c
varshini2304 <varshini0235@gmail.com>
2025-04-14 22:20:53 +0530

    ready to deploy

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,8 +0,0 @@
-import { getDataConnect, validateArgs } from 'firebase/data-connect';
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'nexether',
-  location: 'us-central1'
-};
-
